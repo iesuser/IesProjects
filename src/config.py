1131de346558340daeb7e1b451f67cbb1da7e986
R-Grigala <r.grigalashvili777@gmail.com>
2024-09-19 17:28:02 +0400

    Deployed Flask app in Docker

--- src/config.py
+++ src/config.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+from os import path, sep, pardir
 
 
 # Load environment variables from a custom path
@@ -7,3 +8,4 @@ load_dotenv(dotenv_path='/.env')  # Adjust the path as necessary
 
 class Config(object):
     SECRET_KEY = os.getenv('MY_SECRET_KEY', 'default_secret_key')
+    BASE_DIR = path.abspath(path.dirname(__file__) + sep + pardir)
