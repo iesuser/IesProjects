72a19461b1bc086197ad0761a6804528b5b963d5
varsimashviliLuka <varsimashvili.official@gmail.com>
2025-01-16 16:13:52 +0400

    authentification update

--- src/api/authentication.py
+++ src/api/authentication.py
@@ -1,5 +1,5 @@
 from flask_restx import Resource
-from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, get_jwt_identity
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required, get_jwt_identity, current_user
 
 from src.models import User, Role
 from src.api.nsmodels import auth_ns, registration_parser, auth_parser
@@ -9,8 +9,16 @@ from src.api.nsmodels import auth_ns, registration_parser, auth_parser
 @auth_ns.route('/registration')
 @auth_ns.doc(responses={200: 'OK', 400: 'Invalid Argument', 401: 'JWT Token Expires', 403: 'Forbidden', 404: 'Not Found'})
 class RegistrationApi(Resource):
+    @jwt_required()
     @auth_ns.doc(parser=registration_parser)
+    @auth_ns.doc(security='JsonWebToken')
     def post(self):
+        
+        # Validate if the current user which is trying to register new account has permission for that.
+        if not (current_user.check_permission('is_admin') or current_user.check_permission('can_users')):
+            return {"error": "არ გაქვს მომხმარებლის რეგისტრაციის ნებართვა."}, 403
+
+
         args = registration_parser.parse_args()
 
         # Validate password length and pattern
