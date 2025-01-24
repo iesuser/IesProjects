4c33a17d8cc78622c513cfbb10db9742e1efb463
R-Grigala <r.grigalashvili777@gmail.com>
2025-01-24 14:27:46 +0400

    Change lat long

--- src/api/nsmodels/geophysic_details.py
+++ src/api/nsmodels/geophysic_details.py
@@ -24,10 +24,10 @@ geophysic_seismic_model = api.model('GeophysicSeismic', {
 
 geophysical_seismic__parser = reqparse.RequestParser()
 
-geophysical_seismic__parser.add_argument('first_latitude', type=float, required=True,  help="The latitude of the seismic profile: 43.513")
-geophysical_seismic__parser.add_argument('first_longitude', type=float, required=True,  help="The longitude of the seismic profile: 41.4256")
-geophysical_seismic__parser.add_argument('second_latitude', type=float, required=True,  help="The latitude of the seismic profile: 43.613")
-geophysical_seismic__parser.add_argument('second_longitude', type=float, required=True,  help="The longitude of the seismic profile: 41.5256")
+geophysical_seismic__parser.add_argument('first_latitude', type=str, required=True,  help="The latitude of the seismic profile: 43.513")
+geophysical_seismic__parser.add_argument('first_longitude', type=str, required=True,  help="The longitude of the seismic profile: 41.4256")
+geophysical_seismic__parser.add_argument('second_latitude', type=str, required=True,  help="The latitude of the seismic profile: 43.613")
+geophysical_seismic__parser.add_argument('second_longitude', type=str, required=True,  help="The longitude of the seismic profile: 41.5256")
 geophysical_seismic__parser.add_argument('profile_length', type=float, required=True,  help="The profile length: 100")
 geophysical_seismic__parser.add_argument('vs30', type=int, required=True,  help="VS30 Value example: 600")
 geophysical_seismic__parser.add_argument('ground_category_geo', type=str, required=True, help='Geological ground category: II')
@@ -55,8 +55,8 @@ geophysic_logging_model = geophysic_logging_ns.model('GeophysicLogging', {
 geophysic_logging_parser = reqparse.RequestParser()
 
 geophysic_logging_parser.add_argument('longitude', type=float, required=True,  help="The longitude of the seismic profile: 41.4256")
-geophysic_logging_parser.add_argument('latitude', type=float, required=True,  help="The latitude of the seismic profile: 43.513")
-geophysic_logging_parser.add_argument('profile_length', type=float, required=True,  help="The profile length: 100")
+geophysic_logging_parser.add_argument('latitude', type=str, required=True,  help="The latitude of the seismic profile: 43.513")
+geophysic_logging_parser.add_argument('profile_length', type=str, required=True,  help="The profile length: 100")
 geophysic_logging_parser.add_argument("archival_img", required=False, type=FileStorage, location="files", action="append", help="Upload Images (JPEG/PNG/JPG)")
 geophysic_logging_parser.add_argument("archival_excel", required=False, type=FileStorage, location="files", action="append", help="Upload archival EXCEL (XLS/XLSX)")
 geophysic_logging_parser.add_argument("archival_pdf", required=False, type=FileStorage, location="files", action="append", help="Upload archival PDF (PDF)")
@@ -69,8 +69,8 @@ geophysic_electrical_ns = api.namespace('GeophysicElectrical', description='API 
 geophysic_electrical_model = geophysic_electrical_ns.model('GeophysicElectrical', {
     'id': fields.Integer(readOnly=True, description='The unique identifier of a electrical profile'),
     'geophysical_id': fields.Integer(required=True, description='The ID of the related geophysical record'),
-    'longitude': fields.String(required=True, description='The longitude of the electrical profile'),
-    'latitude': fields.String(required=True, description='The latitude of the electrical profile'),
+    'longitude': fields.Float(required=True, description='The longitude of the electrical profile'),
+    'latitude': fields.Float(required=True, description='The latitude of the electrical profile'),
     'profile_length': fields.Float(required=True, description='The profile length'),
     'archival_img': fields.String(description='The URL of the archival image'),
     'archival_excel': fields.String(description='The URL of the archival Excel file'),
